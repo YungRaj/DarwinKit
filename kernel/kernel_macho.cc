c1a63dbf3997a08524424796f06a08674092bbf0
Ilhan Raja <ilhanraja@google.com>
2025-11-30 23:24:05 -0600

    feat: more formatting

--- kernel/kernel_macho.cc
+++ kernel/kernel_macho.cc
@@ -39,13 +39,11 @@ KernelMachO::KernelMachO(Kernel* kernel)
 
 Kext* KernelMachO::KextLoadedAt(Kernel* kernel, xnu::mach::VmAddress address) {
     darwin::DarwinKit* darwinkit = kernel->GetDarwinKit();
-
     return darwinkit->GetKextByAddress(address);
 }
 
 Kext* KernelMachO::KextWithIdentifier(Kernel* kernel, char* kextname) {
     darwin::DarwinKit* darwinkit = kernel->GetDarwinKit();
-
     return darwinkit->GetKextByIdentifier(kextname);
 }
 
@@ -55,170 +53,128 @@ void KernelMachO::ParseLinkedit() {
 
 bool KernelMachO::ParseLoadCommands() {
     xnu::macho::Header64* mh = GetMachHeader();
-
     Size file_size;
-
     size = GetSize();
-
     file_size = size;
 
     UInt8* q = reinterpret_cast<UInt8*>(mh) + sizeof(xnu::macho::Header64);
-
     UInt32 current_offset = sizeof(xnu::macho::Header64);
-
     for (UInt32 i = 0; i < mh->ncmds; i++) {
         struct load_command* load_command =
             reinterpret_cast<struct load_command*>((*this)[current_offset]);
-
         UInt32 cmdtype = load_command->cmd;
         UInt32 cmdsize = load_command->cmdsize;
-
-        if (cmdsize > mh->sizeofcmds - ((uintptr_t)load_command - (uintptr_t)(mh + 1)))
+        if (cmdsize > mh->sizeofcmds - ((uintptr_t)load_command - (uintptr_t)(mh + 1))) {
             return false;
-
+        }
         switch (cmdtype) {
         case LC_SEGMENT_64: {
             ;
             struct segment_command_64* segment_command =
                 reinterpret_cast<struct segment_command_64*>(load_command);
-
             UInt32 nsects = segment_command->nsects;
             UInt32 sect_offset = current_offset + sizeof(struct segment_command_64);
-
             if (segment_command->fileoff > size ||
-                segment_command->filesize > size - segment_command->fileoff)
+                segment_command->filesize > size - segment_command->fileoff) {
                 return false;
-
+            }
             char buffer1[128];
             char buffer2[128];
-
             snprintf(buffer1, 128, "0x%08llx", segment_command->vmaddr);
             snprintf(buffer2, 128, "0x%08llx", segment_command->vmaddr + segment_command->vmsize);
-
             vm_prot_t maxprot = segment_command->maxprot;
-
             char r[24];
             memcpy(r, ((maxprot & VM_PROT_READ) ? "read" : "-"), 24);
-
             char w[24];
             memcpy(w, ((maxprot & VM_PROT_WRITE) ? "write" : "-"), 24);
-
             char x[24];
             memcpy(x, ((maxprot & VM_PROT_EXECUTE) ? "execute" : "-"), 24);
-
             printf("DarwinKit::LC_SEGMENT_64 at 0x%llx (%s/%s/%s) - %s %s to %s \n",
                    segment_command->fileoff, r, w, x, segment_command->segname, buffer1, buffer2);
-
             if (!strcmp(segment_command->segname, "__LINKEDIT")) {
                 linkedit = reinterpret_cast<UInt8*>(segment_command->vmaddr);
                 linkedit_off = segment_command->fileoff;
                 linkedit_size = segment_command->filesize;
             }
-
             int j = 0;
-
-            if (nsects * sizeof(struct section_64) + sizeof(struct segment_command_64) > cmdsize)
+            if (nsects * sizeof(struct section_64) + sizeof(struct segment_command_64) > cmdsize) {
                 return false;
-
+            }
             Segment* segment = new Segment(segment_command);
-
             for (j = 0; j < nsects; j++) {
                 struct section_64* section =
                     reinterpret_cast<struct section_64*>((*this)[sect_offset]);
-
                 char buffer1[128];
                 char buffer2[128];
-
                 snprintf(buffer1, 128, "0x%08llx", section->addr);
                 snprintf(buffer2, 128, "0x%08llx", section->addr + section->size);
-
                 DARWIN_KIT_LOG("DarwinKit::\tSection %d: %s to %s - %s\n", j, buffer1, buffer2,
                            section->sectname);
-
                 if (section->offset > size || section->size > size - section->offset) {
                     return false;
                 }
-
                 sect_offset += sizeof(struct section_64);
             }
-
             segments.push_back(segment);
-
             break;
         }
-
         case LC_SYMTAB: {
             ;
             struct symtab_command* symtab_command =
                 reinterpret_cast<struct symtab_command*>(load_command);
-
             xnu::macho::Nlist64* symtab;
             UInt32 nsyms;
-
             char* strtab;
             UInt32 strsize;
-
             if (symtab_command->stroff > size || symtab_command->symoff > size ||
                 symtab_command->nsyms >
-                    (size - symtab_command->symoff) / sizeof(xnu::macho::Nlist64))
+                    (size - symtab_command->symoff) / sizeof(xnu::macho::Nlist64)) {
                 return false;
-
+            }
             DARWIN_KIT_LOG("DarwinKit::LC_SYMTAB\n");
             DARWIN_KIT_LOG("DarwinKit::\tSymbol Table is at offset 0x%x (%u) with %u entries \n",
                        symtab_command->symoff, symtab_command->symoff, symtab_command->nsyms);
             DARWIN_KIT_LOG("DarwinKit::\tString Table is at offset 0x%x (%u) with size of %u bytes\n",
                        symtab_command->stroff, symtab_command->stroff, symtab_command->strsize);
-
             if (kernel_cache) {
                 symtab = reinterpret_cast<xnu::macho::Nlist64*>(kernel_cache +
                                                                 symtab_command->symoff);
                 nsyms = symtab_command->nsyms;
-
                 strtab = reinterpret_cast<char*>(kernel_cache + symtab_command->stroff);
                 strsize = symtab_command->strsize;
-
                 char buffer1[128];
                 char buffer2[128];
-
                 snprintf(buffer1, 128, "0x%llx", (UInt64)symtab);
                 snprintf(buffer2, 128, "0x%llx", (UInt64)strtab);
-
                 DARWIN_KIT_LOG("DarwinKit::\tSymbol Table address = %s\n", buffer1);
                 DARWIN_KIT_LOG("DarwinKit::\tString Table address = %s\n", buffer2);
-
             } else if (kernel_collection) {
                 symtab = reinterpret_cast<xnu::macho::Nlist64*>(
                     linkedit + (symtab_command->symoff - linkedit_off));
                 nsyms = symtab_command->nsyms;
-
                 strtab =
                     reinterpret_cast<char*>(linkedit + (symtab_command->stroff - linkedit_off));
                 strsize = symtab_command->strsize;
-
             } else {
                 symtab = nullptr;
                 nsyms = 0;
-
                 strtab = nullptr;
                 strsize = 0;
             }
-
-            if (nsyms > 0)
+            if (nsyms > 0) {
                 ParseSymbolTable(symtab, nsyms, strtab, strsize);
-
+            }
             break;
         }
-
         case LC_DYSYMTAB: {
             ;
             struct dysymtab_command* dysymtab_command =
                 reinterpret_cast<struct dysymtab_command*>(load_command);
-
             if (dysymtab_command->extreloff > size ||
                 dysymtab_command->nextrel >
-                    (size - dysymtab_command->extreloff) / sizeof(struct relocation_info))
+                    (size - dysymtab_command->extreloff) / sizeof(struct relocation_info)) {
                 return false;
-
+            }
             DARWIN_KIT_LOG("DarwinKit::LC_DYSYMTAB\n");
             DARWIN_KIT_LOG("DarwinKit::\t%u local symbols at index %u\n", dysymtab_command->ilocalsym,
                        dysymtab_command->nlocalsym);
@@ -228,64 +184,48 @@ bool KernelMachO::ParseLoadCommands() {
                        dysymtab_command->iundefsym);
             DARWIN_KIT_LOG("DarwinKit::\t%u Indirect symbols at offset 0x%x\n",
                        dysymtab_command->nindirectsyms, dysymtab_command->indirectsymoff);
-
             break;
         }
-
         case LC_UUID: {
             ;
             struct uuid_command* uuid_command =
                 reinterpret_cast<struct uuid_command*>(load_command);
-
-            if (uuid_command->cmdsize != sizeof(struct uuid_command))
+            if (uuid_command->cmdsize != sizeof(struct uuid_command)) {
                 return false;
-
+            }
             DARWIN_KIT_LOG("DarwinKit::LC_UUID\n");
             DARWIN_KIT_LOG("DarwinKit::\tuuid = ");
-
-            for (int j = 0; j < sizeof(uuid_command->uuid); j++)
+            for (int j = 0; j < sizeof(uuid_command->uuid); j++) {
                 DARWIN_KIT_LOG("%x", uuid_command->uuid[j]);
-
+            }
             DARWIN_KIT_LOG("\n");
-
             break;
         }
-
         case LC_FUNCTION_STARTS: {
             ;
             struct linkedit_data_command* linkedit =
                 reinterpret_cast<struct linkedit_data_command*>(load_command);
-
             UInt32 dataoff = linkedit->dataoff;
             UInt32 datasize = linkedit->datasize;
-
             DARWIN_KIT_LOG("DarwinKit::LC_FUNCTION_STARTS\n");
             DARWIN_KIT_LOG("DarwinKit::\tOffset = 0x%x Size = 0x%x\n", dataoff, datasize);
-
             break;
         }
-
         case LC_MAIN: {
             ;
             struct entry_point_command* ep =
                 reinterpret_cast<struct entry_point_command*>(load_command);
-
             DARWIN_KIT_LOG("DarwinKit::LC_MAIN\n");
             DARWIN_KIT_LOG("DarwinKit::\tEntry Point = 0x%llx\n", base + ep->entryoff);
-
             entry_point = base + ep->entryoff;
-
             break;
         }
-
         case LC_UNIXTHREAD: {
             ;
             struct unixthread_command* thread_command =
                 reinterpret_cast<struct unixthread_command*>(load_command);
-
             // if(thread_command->flavor != ARM_THREAD_STATE64)
             // return false;
-
             break;
         }
 
@@ -293,20 +233,15 @@ bool KernelMachO::ParseLoadCommands() {
             ;
             struct linkedit_data_command* linkedit =
                 reinterpret_cast<struct linkedit_data_command*>(load_command);
-
             UInt32 dataoff = linkedit->dataoff;
             UInt32 datasize = linkedit->datasize;
-
             DARWIN_KIT_LOG("DarwinKit::LC_DATA_IN_CODE\n");
             DARWIN_KIT_LOG("DarwinKit::\tOffset = 0x%x Size = 0x%x\n", dataoff, datasize);
-
             break;
         }
         }
-
         current_offset += cmdsize;
     }
-
     return true;
 }
 
--- kernel/kext_macho.cc
+++ kernel/kext_macho.cc
@@ -36,9 +36,7 @@ KextMachO::KextMachO(Kernel* kernel, char* name, xnu::mach::VmAddress base)
 #endif
       ) {
     size = 0;
-
     InitWithBase(base, 0);
-
     kmod_info = reinterpret_cast<xnu::KmodInfo*>(GetSymbolAddressByName("_kmod_info"));
 }
 
@@ -58,7 +56,6 @@ KextMachO::KextMachO(Kernel* kernel, char* name, xnu::KmodInfo* kmod_info)
           0
 #endif
       ) {
-
     InitWithBase(kmod_info->address, 0);
 }
 
@@ -70,180 +67,129 @@ void KextMachO::ParseSymbolTable(xnu::macho::Nlist64* symtab, UInt32 nsyms, char
 
     for (int i = 0; i < nsyms; i++) {
         Symbol* symbol;
-
         xnu::macho::Nlist64* nl = &symtab[i];
-
         char* name;
-
         xnu::mach::VmAddress address;
-
         name = &strtab[nl->n_strx];
-
         address = nl->n_value;
-
         symbol =
             new Symbol(this, nl->n_type & N_TYPE, name, address, AddressToOffset(address),
                        SegmentForAddress(address), SectionForAddress(address));
-
         symbolTable->AddSymbol(symbol);
-
         char buffer[128];
-
         snprintf(buffer, 128, "0x%llx", address);
-
         // DARWIN_KIT_LOG("DarwinKit::Symbol %s %s\n", name, buffer);
     }
-
     // DARWIN_KIT_LOG("DarwinKit::MachO::%u syms!\n", nsyms);
 }
 
 bool KextMachO::ParseLoadCommands() {
     struct mach_header_64* mh = GetMachHeader();
-
     char buffer[128];
-
     snprintf(buffer, 128, "0x%llx", (UInt64)(*this)[sizeof(struct mach_header_64)]);
-
     DARWIN_KIT_LOG("DarwinKit::KextMachO::parseLoadCommands() mh + struct mach_header_64 = %s\n", buffer);
-
 #ifdef __arm64__
     size = MachO::GetSize();
 #endif
-
     UInt8* q = reinterpret_cast<UInt8*>(mh) + sizeof(struct mach_header_64);
-
     UInt32 current_offset = sizeof(struct mach_header_64);
-
     DARWIN_KIT_LOG("DarwinKit::KextMachO::mach_header->ncmds = %u\n", mh->ncmds);
-
     for (UInt32 i = 0; i < mh->ncmds; i++) {
         struct load_command* load_command =
             reinterpret_cast<struct load_command*>((*this)[current_offset]);
-
         UInt32 cmdtype = load_command->cmd;
         UInt32 cmdsize = load_command->cmdsize;
-
-        if (cmdsize > mh->sizeofcmds - ((uintptr_t)load_command - (uintptr_t)(mh + 1)))
+        if (cmdsize > mh->sizeofcmds - ((uintptr_t)load_command - (uintptr_t)(mh + 1))) {
             return false;
-
+        }
         switch (cmdtype) {
         case LC_SEGMENT_64: {
             ;
             struct segment_command_64* segment_command =
                 reinterpret_cast<struct segment_command_64*>(load_command);
-
             UInt32 nsects = segment_command->nsects;
             UInt32 sect_offset = current_offset + sizeof(struct segment_command_64);
-
             if (segment_command->vmaddr == GetBase()) {
                 base_offset = segment_command->fileoff;
             }
-
             char buffer1[128];
             char buffer2[128];
-
             snprintf(buffer1, 128, "0x%08llx", segment_command->vmaddr);
             snprintf(buffer2, 128, "0x%08llx", segment_command->vmaddr + segment_command->vmsize);
-
             // DARWIN_KIT_LOG("DarwinKit::LC_SEGMENT_64 at 0x%llx - %s %s to %s \n",
             // segment_command->fileoff,
             //           segment_command->segname, buffer1, buffer2);
-
             if (!strcmp(segment_command->segname, "__LINKEDIT")) {
                 linkedit = reinterpret_cast<UInt8*>(segment_command->vmaddr);
                 linkedit_off = segment_command->fileoff;
                 linkedit_size = segment_command->filesize;
             }
-
             int j = 0;
-
-            if (nsects * sizeof(struct section_64) + sizeof(struct segment_command_64) > cmdsize)
+            if (nsects * sizeof(struct section_64) + sizeof(struct segment_command_64) > cmdsize) {
                 return false;
-
+            }
             Segment* segment = new Segment(segment_command);
-
             for (j = 0; j < nsects; j++) {
                 struct section_64* section =
                     reinterpret_cast<struct section_64*>((*this)[sect_offset]);
-
                 char buffer1[128];
                 char buffer2[128];
-
                 snprintf(buffer1, 128, "0x%08llx", section->addr);
                 snprintf(buffer2, 128, "0x%08llx", section->addr + section->size);
-
                 // DARWIN_KIT_LOG("DarwinKit::\tSection %d: %s to %s - %s\n", j, buffer1, buffer2,
                 //           section->sectname);
-
                 sect_offset += sizeof(struct section_64);
             }
-
             segments.push_back(segment);
-
             break;
         }
-
         case LC_SYMTAB: {
             ;
             struct symtab_command* symtab_command =
                 reinterpret_cast<struct symtab_command*>(load_command);
-
             xnu::macho::Nlist64* symtab;
             UInt32 nsyms;
-
             char* strtab;
             UInt32 strsize;
-
             // DARWIN_KIT_LOG("DarwinKit::LC_SYMTAB\n");
             // DARWIN_KIT_LOG("DarwinKit::\tSymbol Table is at offset 0x%x (%u) with %u entries \n",
             //           symtab_command->symoff, symtab_command->symoff, symtab_command->nsyms);
             // DARWIN_KIT_LOG("DarwinKit::\tString Table is at offset 0x%x (%u) with size of %u bytes\n",
             //          symtab_command->stroff, symtab_command->stroff, symtab_command->strsize);
-
             if (kernel_cache) {
                 symtab = reinterpret_cast<xnu::macho::Nlist64*>(kernel_cache +
                                                                 symtab_command->symoff);
                 nsyms = symtab_command->nsyms;
-
                 strtab = reinterpret_cast<char*>(kernel_cache + symtab_command->stroff);
                 strsize = symtab_command->strsize;
-
                 char buffer1[128];
                 char buffer2[128];
-
                 snprintf(buffer1, 128, "0x%llx", (UInt64)symtab);
                 snprintf(buffer2, 128, "0x%llx", (UInt64)strtab);
-
                 // DARWIN_KIT_LOG("DarwinKit::\tSymbol Table address = %s\n", buffer1);
                 // DARWIN_KIT_LOG("DarwinKit::\tString Table address = %s\n", buffer2);
-
             } else if (kernel_collection) {
                 symtab = reinterpret_cast<xnu::macho::Nlist64*>(
                     GetBase() + (symtab_command->symoff - base_offset));
                 nsyms = symtab_command->nsyms;
-
                 strtab = reinterpret_cast<char*>(GetBase() +
                                                  (symtab_command->stroff - base_offset));
                 strsize = symtab_command->strsize;
             } else {
                 symtab = nullptr;
                 nsyms = 0;
-
                 strtab = nullptr;
                 strsize = 0;
             }
-
             if (nsyms > 0) {
                 ParseSymbolTable(symtab, nsyms, strtab, strsize);
             }
-
             break;
         }
         case LC_DYSYMTAB: {
             ;
             struct dysymtab_command* dysymtab_command =
                 reinterpret_cast<struct dysymtab_command*>(load_command);
-
             /* DARWIN_KIT_LOG("DarwinKit::LC_DYSYMTAB\n");
             DARWIN_KIT_LOG("DarwinKit::\t%u local symbols at index %u\n", dysymtab_command->ilocalsym,
                        dysymtab_command->nlocalsym);
@@ -256,59 +202,44 @@ bool KextMachO::ParseLoadCommands() {
 
             break;
         }
-
         case LC_UUID: {
             ;
             struct uuid_command* uuid_command =
                 reinterpret_cast<struct uuid_command*>(load_command);
-
-            if (uuid_command->cmdsize != sizeof(struct uuid_command))
+            if (uuid_command->cmdsize != sizeof(struct uuid_command)) {
                 return false;
-
+            }
             // DARWIN_KIT_LOG("DarwinKit::LC_UUID\n");
             // DARWIN_KIT_LOG("DarwinKit::\tuuid = ");
-
             for (int j = 0; j < sizeof(uuid_command->uuid); j++)
                 // DARWIN_KIT_LOG("%x", uuid_command->uuid[j]);
-
                 // DARWIN_KIT_LOG("\n");
-
                 break;
         }
-
         case LC_DATA_IN_CODE: {
             ;
             struct linkedit_data_command* linkedit =
                 reinterpret_cast<struct linkedit_data_command*>(load_command);
-
             UInt32 dataoff = linkedit->dataoff;
             UInt32 datasize = linkedit->datasize;
-
             // DARWIN_KIT_LOG("DarwinKit::LC_DATA_IN_CODE\n");
             // DARWIN_KIT_LOG("DarwinKit::\tOffset = 0x%x Size = 0x%x\n", dataoff, datasize);
-
             break;
         }
         }
-
         current_offset += cmdsize;
     }
-
     return true;
 }
 
 void KextMachO::ParseHeader() {
     struct mach_header_64* mh = reinterpret_cast<struct mach_header_64*>(GetMachHeader());
-
     UInt32 magic = mh->magic;
-
     if (magic == FAT_CIGAM) {
-
     } else if (magic == MH_MAGIC_64) {
 #ifdef __x86_64__
         size = kmod_info->size;
 #endif
-
         ParseLoadCommands();
     }
 }
