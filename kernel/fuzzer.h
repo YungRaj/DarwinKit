e5e50e8c3528acc2d63e575b9f7ee437d14297c4
Ilhan Raja <ilhanraja@google.com>
2025-12-01 21:15:02 -0600

    fix: style for thunks

--- kernel/fuzzer.h
+++ kernel/fuzzer.h
@@ -7,9 +7,6 @@
 extern "C" {
 #endif
 
-extern void sanitizer_cov_enable_coverage();
-extern void sanitizer_cov_disable_coverage();
-
 int LibAFLFuzzerTestOneInput(uint8_t *data, size_t size);
 #ifdef __cplusplus
 }
